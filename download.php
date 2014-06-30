ebfd5063698168c6a0947e3536f9afb38a7a15c4
puritys <pipi00000@gmail.com>
2014-06-30 20:04:31 +0800

    modify

--- download.php
+++ download.php
@@ -4,7 +4,7 @@ $url = $_POST['url'];
 $name = $_POST['name'];
 require_once "webClient.php";
 $name = strip_tags($name);
-$name = preg_replace('/[^0-9a-z]/i', '', $name);
+$name = preg_replace('/[\/\\\\.]/i', '', $name);
 
 $c = new MyCurl();
 $saveToPath = dirname(__FILE__) . '/other/'. $name.'.mp3';
